b65ffc2ca1e6057a9b961dac658ba6996bd37348
maxi758 <maxi758@gmail.com>
2023-12-11 15:58:51 -0300

    initiial server connfig

---
+++ src/app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.sjtjxec.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => {
+    app.listen(5000, () => {
+      console.log('Server running on port 5000');
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
