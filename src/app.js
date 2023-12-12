0a230cee2aed9f1b696dfe1f37cd0af3b4dc7a20
maxi758 <maxi758@gmail.com>
2023-12-12 15:31:22 -0300

    add error handler

--- src/app.js
+++ src/app.js
@@ -16,6 +16,19 @@ app.get('/', (req, res) => {
 
 app.use('/api/especialidades', especialidadRoutes);
 
+app.use((req, res, next) => {
+  const error = new HttpError('Could not find this route', 404);
+  throw error;
+});
+
+app.use((error, req, res, next) => {
+  if (res.headerSent) {
+    return next(error);
+  }
+  res.status(error.code || 500);
+  res.json({ message: error.message || 'An unknow error has ocurred' });
+});
+
 mongoose
   .connect(
     `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.sjtjxec.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
