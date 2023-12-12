e568aca7cdcdbe388cfcb19066f922c6201acdf3
maxi758 <maxi758@gmail.com>
2023-12-12 15:59:53 -0300

    add medico controller methods

--- src/controllers/medicos-controller.js
+++ src/controllers/medicos-controller.js
@@ -22,3 +22,69 @@ const getMedicos = async (req, res, next) => {
     medicos: medicos.map((medico) => medico.toObject({ getters: true })),
   });
 };
+
+const getMedicoById = async (req, res, next) => {
+  const { id } = req.params;
+  let medico;
+  try {
+    medico = await Medico.findById(id);
+  } catch (err) {
+    const error = new HttpError(
+      'Error en la consulta, intente de nuevo más tarde',
+      500
+    );
+  }
+
+  if (!medico) {
+    return next(new HttpError('No se encontró un médico para el id dado', 404));
+  }
+
+  res.json({ medico });
+};
+//Admin
+const createMedico = async (req, res, next) => {
+  const medico = new Medico({
+    ...req.body,
+  });
+
+  try {
+    await medico.save();
+  } catch (err) {
+    const error = new HttpError(
+      'No se pudo crear el médico, intente de nuevo más tarde',
+      500
+    );
+    return next(error);
+  }
+
+  res.status(201).json({ medico: medico.toObject({ getters: true }) });
+};
+
+//Admin
+
+const updateMedico = async (req, res, next) => {
+  const { id } = req.params;
+  const propertiesToUpdate = req.body;
+  let medico;
+  try {
+    medico = Medico.findByIdAndUpdate(id, propertiesToUpdate, { new: true });
+  } catch (err) {
+    const error = new HttpError(
+      'No se pudieron actualizar los datos del médico, intente de nuevo más tarde',
+      500
+    );
+    return next(error);
+  }
+
+  if (!medico) {
+    return next(new HttpError('No se encontró un médico para el id dado', 404));
+  }
+  res.json({ medico });
+};
+
+module.exports = {
+  getMedicos,
+  getMedicoById,
+  createMedico,
+  updateMedico,
+};
