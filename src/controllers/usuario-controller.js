0fc284b74bcd1962b22aaf380aa3fbcf2e1e338b
maxi758 <maxi758@gmail.com>
2023-12-13 14:55:55 -0300

    add usuario methods

---
+++ src/controllers/usuario-controller.js
@@ -0,0 +1,64 @@
+const HttpError = require('../models/http-error');
+
+const Usuario = require('../models/usuario');
+
+const getUsuarios = async (req, res, next) => {
+  let usuarios;
+  try {
+    usuarios = await Usuario.find();
+  } catch (err) {
+    const error = new HttpError(
+      'Error en la consulta, intente de nuevo más tarde',
+      500
+    );
+    return next(error);
+  }
+
+  if (!usuarios || usuarios.length === 0) {
+    return next(new HttpError('No se encontraron usuarios', 404));
+  }
+
+  res.json({
+    usuarios: usuarios.map((usuario) => usuario.toObject({ getters: true })),
+  });
+};
+
+const createUsuario = async (req, res) => {
+  const usuario = new Usuario(req.body);
+
+  try {
+    await usuario.save();
+    console.log(usuario);
+    //sendWelcomeEmail(usuario.email, usuario.name); // send welcome email
+    const token = await usuario.generateAuthToken();
+    res.status(201).send({ usuario, token }); // send back user and token
+  } catch (err) {
+    const error = new HttpError(
+      'Error en la consulta, intente de nuevo más tarde',
+      500
+    );
+  }
+};
+
+const login = async (req, res) => {
+  try {
+    const usuario = await Usuario.findByCredentials(
+      req.body.email,
+      req.body.password
+    );
+    const token = await usuario.generateAuthToken();
+
+    res.send({ usuario, token });
+  } catch (err) {
+    const error = new HttpError(
+      'Error en la consulta, intente de nuevo más tarde',
+      500
+    );
+  }
+};
+
+module.exports = {
+  getUsuarios,
+  createUsuario,
+  login,
+};
--- src/models/usuario.js
+++ src/models/usuario.js
@@ -12,4 +12,50 @@ const usuarioSchema = new Schema({
   turnos: [{ type: mongoose.Types.ObjectId, required: false, ref: 'Turno' }],
 });
 
+usuarioSchema.methods.generateAuthToken = async function () {
+  const user = this;
+
+  const token = jwt.sign({ _id: user._id.toString() }, 'mysecret', {
+    expiresIn: '360 minutes',
+  });
+
+  user.tokens = user.tokens.concat({ token }); // add token to user's tokens array
+
+  await user.save();
+
+  return token;
+};
+
+usuarioSchema.statics.findByCredentials = async (email, password) => {
+  // statics are accessible on the model
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw new Error('Unable to login');
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password); // compare plain text password to hashed password
+  // console.log(isMatch)
+
+  if (!isMatch) {
+    throw new Error('Unable to login');
+  }
+
+  return user;
+};
+
+usuarioSchema.pre('save', async function (next) {
+  const user = this;
+
+  if (user.isModified('password')) {
+    // true if password is being modified
+    user.password = await bcrypt.hash(user.password, 8);
+  }
+
+  // console.log('just before saving!')
+
+  next();
+});
+
+
 module.exports = mongoose.model('Usuario', usuarioSchema);
