963302a1cc22ebec80de1300baf59a01f22980e9
maxi758 <maxi758@gmail.com>
2023-12-14 15:57:16 -0300

    add auth

--- src/routes/medico-route.js
+++ src/routes/medico-route.js
@@ -8,13 +8,14 @@ const {
   createMedico,
   updateMedico,
 } = require('../controllers/medicos-controller');
+const auth = require('../middleware/auth');
 
 router.get('/', getMedicos);
 
 router.get('/:id', getMedicoById);
 
-router.post('/', createMedico);
+router.post('/', (req, res, next) => auth('ADMIN', req, res, next) ,createMedico);
 
-router.patch('/:id', updateMedico);
+router.patch('/:id', (req, res, next) => auth('ADMIN', req, res, next), updateMedico);
 
 module.exports = router;
